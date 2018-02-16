b261a01c30de0de006f2c1fa58522238759dc9cc
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-02-16 10:02:29 +0700

    Created empty fragment for history.

--- app/src/main/java/com/example/android/runvolution/MainActivity.java
+++ app/src/main/java/com/example/android/runvolution/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.runvolution;
 
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,6 +12,19 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("");
+                    loadHistoryFragment();
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -31,14 +47,20 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private void loadHistoryFragment() {
+        Fragment historyFragment = fragmentManager.findFragmentByTag("history");
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (historyFragment == null) {
+            historyFragment = new HistoryFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, historyFragment, "history")
+                    .commit();
+        } else {
+            fragmentManager
+                    .beginTransaction()
+                    .attach(historyFragment)
+                    .commit();
+        }
     }
-
 }
