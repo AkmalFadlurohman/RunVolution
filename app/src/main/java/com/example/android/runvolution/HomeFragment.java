dfd55910fdcdb15ef7ad7925b35a2c7a7b83abb7
fariz.tumbuan <13515050@std.stei.itb.ac.id>
2018-02-17 08:24:10 +0800

    Added dummy text and functional basic timer in home fragment

--- app/src/main/java/com/example/android/runvolution/HomeFragment.java
+++ app/src/main/java/com/example/android/runvolution/HomeFragment.java
@@ -2,10 +2,16 @@ package com.example.android.runvolution;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.android.runvolution.home.Timer;
 
 
 /**
@@ -13,11 +19,12 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-
+    public TextView timerTextView;
+    public Button timerButton;
+    private Timer timer;
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +33,35 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        timerButton = (Button) getView().findViewById(R.id.timerButton);
+        timerTextView = (TextView) getView().findViewById(R.id.timerView);
+        timer = new Timer(timerTextView);
+        timerButton.setText("START");
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = (Button) view;
+                if (button.getText().equals("STOP")) {
+                    timer.timerHandler.removeCallbacks(timer.timerRunnable);
+                    button.setText("START");
+                } else {
+                    timer.startTime = System.currentTimeMillis();
+                    timer.timerHandler.postDelayed(timer.timerRunnable, 0);
+                    button.setText("STOP");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        timer.timerHandler.removeCallbacks(timer.timerRunnable);
+        Button button = (Button) getView().findViewById(R.id.timerButton);
+        button.setText("START");
+    }
 }
