996173b15e0249b73a92fa012e5b948160342ad8
Akmal Fadlurohman <fadlurohmanakmal@rocketmail.com>
2018-02-19 19:58:47 +0700

    Add shared preference to save login state

--- app/src/main/java/com/AlForce/android/runvolution/LoginActivity.java
+++ app/src/main/java/com/AlForce/android/runvolution/LoginActivity.java
@@ -6,9 +6,11 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -192,8 +194,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
+        @NonNull
         private final String email;
+        @NonNull
         private final String password;
 
         UserLoginTask(String email, String password) {
@@ -260,7 +263,14 @@ public class LoginActivity extends AppCompatActivity {
             showProgress(false);
 
             if (success) {
-                Toast.makeText(LoginActivity.this, "Succesfully logged in",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(LoginActivity.this, "Succesfully logged in",Toast.LENGTH_SHORT).show();
+                SharedPreferences preferences = getSharedPreferences(getString(R.string.sharedpref_file), MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("logged",true);
+                editor.putString("email",email);
+                editor.putString("password",password);
+                editor.apply();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             } else {
                 passwordView.setError(getString(R.string.error_incorrect_password));
                 passwordView.requestFocus();
--- app/src/main/java/com/AlForce/android/runvolution/SplashActivity.java
+++ app/src/main/java/com/AlForce/android/runvolution/SplashActivity.java
@@ -2,6 +2,7 @@ package com.AlForce.android.runvolution;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,8 +14,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.sharedpref_file), MODE_PRIVATE);
+        Boolean logged = preferences.getBoolean("logged", false);
+        if (logged) {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
         } else {
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
