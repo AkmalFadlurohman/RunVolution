65736592076a4b09794d0a362eff79ee94c997d4
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-02-20 23:26:54 +0700

    Reimplemented shake detector.

--- app/src/main/java/com/AlForce/android/runvolution/MainActivity.java
+++ app/src/main/java/com/AlForce/android/runvolution/MainActivity.java
@@ -1,5 +1,8 @@
 package com.AlForce.android.runvolution;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,28 +10,85 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.AlForce.android.runvolution.sensor.ShakeDetector;
+import com.AlForce.android.runvolution.utils.DatabaseOpenHelper;
 import com.AlForce.android.runvolution.utils.FragmentFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG_ACTIVITY =
+    public static final String TAG =
             MainActivity.class.getSimpleName();
+    public static final String TAB_HOME = FragmentFactory.TAG_FRAGMENT_HOME;
+    public static final String TAB_HISTORY = FragmentFactory.TAG_FRAGMENT_HISTORY;
+    public static final String TAB_STATUS = FragmentFactory.TAG_FRAGMENT_STATUS;
+    public static final String[] TABS = {
+            TAB_HOME, TAB_HISTORY, TAB_STATUS
+    };
 
+    private DatabaseOpenHelper dbHelper;
     private FragmentManager fragmentManager;
+
+    /* Shake Detection Variables*/
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
+        dbHelper = new DatabaseOpenHelper(this);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        loadFragment(FragmentFactory.TAG_FRAGMENT_HOME);
+        fragmentManager = getSupportFragmentManager();
+        loadFragment(TAB_HOME);
+
+        initializeShakeDetector();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(
+                mShakeDetector,
+                mAccelerometer,
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(mShakeDetector);
+    }
+
+    private void initializeShakeDetector() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        ShakeDetector.OnShakeListener shakeListener = createShakeListener();
+        mShakeDetector.setOnShakeListener(shakeListener);
+    }
+
+    private ShakeDetector.OnShakeListener createShakeListener() {
+        ShakeDetector.OnShakeListener listener =
+                new ShakeDetector.OnShakeListener() {
+                    @Override
+                    public void onShake(int count) {
+                        handleShakeEvent(count);
+                    }
+
+                    private void handleShakeEvent(int count) {
+                        moveTaskToBack(true);
+                    }
+                };
+
+        return listener;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -38,13 +98,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    loadFragment(FragmentFactory.TAG_FRAGMENT_HOME);
+                    loadFragment(TAB_HOME);
                     return true;
                 case R.id.navigation_history:
-                    loadFragment(FragmentFactory.TAG_FRAGMENT_HISTORY);
+                    loadFragment(TAB_HISTORY);
                     return true;
                 case R.id.navigation_notifications:
-                    loadFragment(FragmentFactory.TAG_FRAGMENT_STATUS);
+                    loadFragment(TAB_STATUS);
                     return true;
             }
             return false;
