7cf7073d3d8c6c70803e0224ea6dbe9a580c90f2
Akmal Fdlurohman <fadlurohmanakmal@rocketmail.com>
2018-02-22 23:21:46 +0700

    Add handler to update pet name on preference edit event

--- app/src/main/java/com/AlForce/android/runvolution/SettingsActivity.java
+++ app/src/main/java/com/AlForce/android/runvolution/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.AlForce.android.runvolution;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
@@ -8,20 +10,35 @@ import android.content.res.Configuration;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -35,9 +52,8 @@ import java.util.List;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class SettingsActivity extends AppCompatPreferenceActivity {
-
-
-
+    private PetNameUpdaterTask updateTask = null;
+    private static Context settingsContext;
 
     /**
      * Helper method to determine if the device has an extra-large screen. For
@@ -48,29 +64,15 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
 
-    /**
-     * Binds a preference's summary to its value. More specifically, when the
-     * preference's value is changed, its summary (line of text below the
-     * preference title) is updated to reflect the value. The summary is also
-     * immediately updated upon calling this method. The exact display format is
-     * dependent on the type of preference.
-     *
-     *
-     */
-    /*private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        SharedPreferences preferences = getSharedPreferences(getString(R.string.sharedpref_file), MODE_PRIVATE);
-        //sBindPreferenceSummaryToValueListener.onPreferenceChange(preference, preferences.getString(preference.getKey(),""));
-    }*/
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        settingsContext = this.getApplicationContext();
         setupActionBar();
+    }
+
+    public static Context getAppContext() {
+        return settingsContext;
     }
 
     /**
@@ -168,8 +170,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                     preferences.edit().remove("logged").apply();
                     preferences.edit().remove("name").apply();
                     preferences.edit().remove("email").apply();
-                    Intent signoutIntent = new Intent(getActivity(), LoginActivity.class);
-                    startActivity(signoutIntent);
                     return true;
                 }
             });
@@ -198,10 +198,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             Preference.OnPreferenceChangeListener bindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object value) {
-                    String stringValue = value.toString();
+                    String newPetName = value.toString();
                     SharedPreferences preferences = PetPreferenceFragment.this.getActivity().getSharedPreferences(getString(R.string.sharedpref_file), MODE_PRIVATE);
-                    preferences.edit().putString(preference.getKey(),stringValue).apply();
-                    preference.setSummary(stringValue);
+                    if (isPetNameValid(newPetName)) {
+                        preferences.edit().putString(preference.getKey(),newPetName).apply();
+                        preference.setSummary(newPetName);
+                        int petId = preferences.getInt("petId",0);
+                        new PetNameUpdaterTask(petId,newPetName).execute((Void) null);
+                    } else {
+                        preference.setSummary("Please fill a proper pet name");
+                    }
                     return true;
                 }
             };
@@ -215,14 +221,13 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             petNamePref.setSummary(petName);
             petNamePref.setOnPreferenceChangeListener(bindPreferenceSummaryToValueListener);
             bindPreferenceSummaryToValueListener.onPreferenceChange(petNamePref, preferences.getString("petName",""));
-            /*petNamePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    petNamePref.setSummary(petNamePref.get);
-                }
-            }*/
         }
+
+
         public Boolean isPetNameValid(String petName) {
+            if (petName.length() == 0 || petName.matches(".*\\d+.*")) {
+                return false;
+            }
             return true;
         }
         @Override
@@ -233,6 +238,82 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 return true;
             }
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class PetNameUpdaterTask extends AsyncTask<Void, Void, Boolean> {
+        private final int petID;
+        @NonNull
+        private final String newName;
+
+        PetNameUpdaterTask(int petID, String newName) {
+            this.petID = petID;
+            this.newName = newName;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            String address = "https://runvolution.herokuapp.com/updatepetname";
+            String param = "?petid=" + petID + "&name=" + newName;
+            HttpsURLConnection httpsPatch = null;
+            BufferedReader reader = null;
+            String msg = null;
+
+            try {
+                URL urlAddress = new URL(address + param);
+                httpsPatch = (HttpsURLConnection) urlAddress.openConnection();
+                httpsPatch.setRequestMethod("PATCH");
+                httpsPatch.connect();
+                reader = new BufferedReader(new InputStreamReader(httpsPatch.getInputStream()));
+                String inputLine;
+                StringBuilder buffer = new StringBuilder();
+                int respCode = httpsPatch.getResponseCode();
+                while ((inputLine = reader.readLine()) != null) {
+                    buffer.append(inputLine);
+                }
+                msg = buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } finally {
+                if (httpsPatch != null) {
+                    httpsPatch.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (msg == null) {
+                    Log.d("The server responded with : ", "Message is null");
+                } else {
+                    Log.d("The server responded with : ", msg);
+                }
+
+            }
+            if (msg != null) {
+                return "OK".equals(msg);
+            } else {
+                return false;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                Toast.makeText(SettingsActivity.getAppContext(), "Pet Name Updated", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(SettingsActivity.getAppContext(), "Failed to Update Pet Name", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            //updateTask = null;
         }
     }
 }
