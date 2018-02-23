161ae1e0d407229fe69e0b2750d406325b69a129
Akmal Fdlurohman <fadlurohmanakmal@rocketmail.com>
2018-02-23 16:28:36 +0700

    Add handler to update user record

--- app/src/main/java/com/AlForce/android/runvolution/HomeFragment.java
+++ app/src/main/java/com/AlForce/android/runvolution/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.AlForce.android.runvolution;
 
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.AlForce.android.runvolution.history.HistoryDAO;
 import com.AlForce.android.runvolution.history.HistoryItem;
@@ -19,7 +22,15 @@ import com.AlForce.android.runvolution.timer.Timer;
 import com.AlForce.android.runvolution.utils.DatabaseOpenHelper;
 import com.AlForce.android.runvolution.utils.DatabaseUpdateListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Date;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -96,6 +107,9 @@ public class HomeFragment extends Fragment {
             public void onDatabaseUpdate() {
                 totalDistance = statistics.getTotalDistance();
                 totalDistanceTextView.setText(Float.toString(totalDistance));
+                SharedPreferences preferences = HomeFragment.this.getActivity().getSharedPreferences(getString(R.string.sharedpref_file), MODE_PRIVATE);
+                String email = preferences.getString("email",null);
+                //new RecordUpdaterTask(email,totalDistance).execute((Void) null)
             }
         };
         historyDAO.setListener(updateListener);
@@ -118,10 +132,9 @@ public class HomeFragment extends Fragment {
 
     private void saveCurrentRecord() {
         HistoryItem record = new HistoryItem();
-        record.setDate(new Date());
+        record.setDate(new Date(new Date().getTime()));
         record.setDistance(currentDistance);
         record.setSteps(currentSteps);
-
         long newId = historyDAO.insert(record);
         Log.d(TAG, "saveCurrentRecord: Created item with id="+newId);
     }
@@ -132,5 +145,83 @@ public class HomeFragment extends Fragment {
         timer.timerHandler.removeCallbacks(timer.timerRunnable);
         Button button = (Button) getView().findViewById(R.id.timerButton);
         button.setText("START");
+    }
+
+    public static class RecordUpdaterTask extends AsyncTask<Void, Void, Boolean> {
+        @NonNull
+        private final String email;
+        private final float record;
+
+        RecordUpdaterTask(String email, float record) {
+            this.email = email;
+            this.record = record;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            String address = "https://runvolution.herokuapp.com/updaterecord";
+            String param = "?email=" + email + "&record=" + record;
+            HttpsURLConnection httpsPatch = null;
+            BufferedReader reader = null;
+            String msg = null;
+
+            try {
+                URL urlAddress = new URL(address + param);
+                httpsPatch = (HttpsURLConnection) urlAddress.openConnection();
+                httpsPatch.setRequestMethod("PATCH");
+                httpsPatch.connect();
+                reader = new BufferedReader(new InputStreamReader(httpsPatch.getInputStream()));
+                String inputLine;
+                StringBuilder buffer = new StringBuilder();
+                int respCode = httpsPatch.getResponseCode();
+                while ((inputLine = reader.readLine()) != null) {
+                    buffer.append(inputLine);
+                }
+                msg = buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } finally {
+                if (httpsPatch != null) {
+                    httpsPatch.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (msg == null) {
+                    Log.d("The server responded with : ", "Message is null");
+                } else {
+                    Log.d("The server responded with : ", msg);
+                }
+
+            }
+            if (msg != null) {
+                return "OK".equals(msg);
+            } else {
+                return false;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                Log.d(TAG, "Updated user record on server database");
+                //Toast.makeText(HomeFragment.this.getActivity().getApplicationContext(), "Congratulations, You just achieved a new record", Toast.LENGTH_SHORT).show();
+            } else {
+                Log.d(TAG,"Failed to update record");
+                //Toast.makeText(HomeFragment.this.getActivity().getApplicationContext(), "Update record failed", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            //Empty
+        }
     }
 }
